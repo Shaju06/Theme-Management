b53710f64c703a4bddabffe6618aeba5c90f6d26
Varinder <Varipaul@gmail.com>
2023-08-23 22:02:47 +0530

    commit

--- server.js
+++ server.js
@@ -17,92 +17,88 @@ const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 5000; 
-// app.use(bodyParser.json());
-// app.use(dbConnection)
-// setApplicationHeaderOptions(app)
+app.use(bodyParser.json());
+app.use(dbConnection)
+setApplicationHeaderOptions(app)
 
-// // API routes
-// app.post('/api/login', async (req, res) => {
+// API routes
+app.post('/api/login', async (req, res) => {
 
-//     const {userName, password } = req.body
+    const {userName, password } = req.body
     
-//     try {
-//       const userCollection = req.collection; 
+    try {
+      const userCollection = req.collection; 
       
-//         const user = await userCollection.findOne({ userName: userName });
+        const user = await userCollection.findOne({ userName: userName });
 
-//         console.log(user, req.body)
+        console.log(user, req.body)
        
-//         if (!user) {
-//           return res.status(401).send({ message: 'Invalid user/password'});
-//         }
+        if (!user) {
+          return res.status(401).send({ message: 'Invalid user/password'});
+        }
     
-//         const validPassword = await bcrypt.compare(password, user.password);
-//         if (!validPassword) {
-//           return res.status(401).send({messgae: 'Invalid user/password'});
-//         }
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+          return res.status(401).send({messgae: 'Invalid user/password'});
+        }
 
-//         var token = jwt.sign(req.body, 'shhhhh');
+        var token = jwt.sign(req.body, 'shhhhh');
     
-//         res.status(200).send({message: 'Login successful.', token, primaryColor: user.primaryColor});
-//       } catch (error) {
-//         console.log(error)
-//         res.status(500).send('Error logging in.');
-//       }
-// });
+        res.status(200).send({message: 'Login successful.', token, primaryColor: user.primaryColor});
+      } catch (error) {
+        console.log(error)
+        res.status(500).send('Error logging in.');
+      }
+});
 
-// app.patch('/api/update/',verifyToken, async (req, res) => {
+app.patch('/api/update/',verifyToken, async (req, res) => {
   
-//   const {primaryColor} = req.body
-//   const userCollection = req.collection
-//   const {userName} = req.user
+  const {primaryColor} = req.body
+  const userCollection = req.collection
+  const {userName} = req.user
 
-//   console.log(req.body)
+  console.log(req.body)
 
-//   try {
+  try {
     
-//     const updatedUser = await userCollection.findOneAndUpdate({userName}, { $set: {primaryColor} }, { new: true });
+    const updatedUser = await userCollection.findOneAndUpdate({userName}, { $set: {primaryColor} }, { new: true });
   
-//     if (!updatedUser) {
-//       return res.status(404).json({ message: 'User not found' });
-//     }
+    if (!updatedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
 
-//     return res.json({message: 'Data updated', primaryColor: updatedUser.value.primaryColor});
-//   } catch (error) {
-//     return res.status(500).json({ message: 'Error updating user', error: error.message });
-//   }
-// });
+    return res.json({message: 'Data updated', primaryColor: updatedUser.value.primaryColor});
+  } catch (error) {
+    return res.status(500).json({ message: 'Error updating user', error: error.message });
+  }
+});
 
-// app.post('/api/register', async (req, res) => {
-//   try {
-//     const { userName, password, primaryColor } = req.body;
-//     const userCollection = req.collection
-//     const salt = 10
+app.post('/api/register', async (req, res) => {
+  try {
+    const { userName, password, primaryColor } = req.body;
+    const userCollection = req.collection
+    const salt = 10
 
-//     // Hash the password
-//     const hashedPassword = await bcrypt.hash(password, salt);
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-//     await userCollection.insertOne({ userName, password: hashedPassword, primaryColor: primaryColor });
+    await userCollection.insertOne({ userName, password: hashedPassword, primaryColor: primaryColor });
 
-//     res.status(201).json({ message: 'User registered successfully.' });
-//   } catch (error) {
-//     console.log(error)
-//     res.status(500).json({ message: 'An error occurred.' });
-//   }
-// });
+    res.status(201).json({ message: 'User registered successfully.' });
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'An error occurred.' });
+  }
+});
 
 
-// // All other routes should serve the React app
-// if(process.env.NODE_ENV === 'production'){
-// app.use(express.static(path.join(__dirname, '/client/build')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/client/build/index.html'));
-// });
-// }
-
-app.get('/', (req, res) => {
-  res.send("fdfd")
-})
+// All other routes should serve the React app
+if(process.env.NODE_ENV === 'production'){
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
